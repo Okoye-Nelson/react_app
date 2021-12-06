f6c4dd65e5d6a6b4e12288dcb682f23d0bedd6b9
Nelson Okoye <nlsnokoye@gmail.com>
2021-12-06 16:37:16 +0100

    changes

--- src/App.test.js
+++ src/App.test.js
@@ -11,7 +11,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders welcome message', () => {
+it('renders welcome message', () => {
   render(<App />);
   expect(screen.getByText('Learn React')).toBeInTheDocument();
 });
