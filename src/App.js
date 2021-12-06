28cefe7c211be6af1eab7a3dd52d21a2f8f32e05
Nelson Okoye <nlsnokoye@gmail.com>
2021-12-06 14:28:08 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Update APP after running react_app pipeline in circleCI.
+          Update after running react_app pipeline in circleCI.
           </p>
         <a 
         className="App-link" href="https://reactjs.org" 
